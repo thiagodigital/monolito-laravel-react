46b4ee5fbc8c6098af6460e754e595a9b4f63740
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-07-18 18:12:26 -0300

    [Monolito]Laravel+React+Typescript

---
+++ resources/js/Pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+    let foo: string = "React";
+    const bar: string = "TypeScript";
+
+    return (
+        <>
+            <h1>
+                Hello World!!!
+            </h1>
+            <p>
+                {foo} + {bar} + Laravel
+            </p>
+        </>
+    );
+};
+
+export default Home;
